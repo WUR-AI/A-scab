b3360f1dfec68fd6ccb4006407815007f6637cce
Michiel Kallenberg <michielkallenberg@gmail.com>
2024-07-08 16:40:31 +0200

    add agents

--- ascab/train.py
+++ ascab/train.py
@@ -11,22 +11,21 @@ np.set_printoptions(precision=2, suppress=True)
 def rl_agent():
     ascab = FlattenObservation(AScabEnv())
     model = PPO("MlpPolicy", ascab, verbose=1, seed=42)
-    model.learn(total_timesteps=1000)
-    reward_per_episode, _ = evaluate_policy(model, ascab, n_eval_episodes=1)
-    print(f'{reward_per_episode}')
-    
+    model.learn(total_timesteps=50000)
+    evaluate_with_sb = True
+    if evaluate_with_sb:
+        reward_per_episode, _ = evaluate_policy(model, ascab, n_eval_episodes=1)
+        print(f'{reward_per_episode}')
+
     terminated = False
     total_reward = 0.0
-    observation = model.env.reset()
-    ascab_ = FlattenObservation(AScabEnv())
-    ascab_.reset()
+    observation, _ = ascab.reset()
     while not terminated:
-        action, _state = model.predict(observation, deterministic=True)
-        observation, reward, terminated, info = model.env.step(action)
-        ascab_.step(action)
+        action_ = model.predict(observation, deterministic=True)[0]
+        observation, reward, terminated, _, _ = ascab.step(action_)
         total_reward += reward
     print(f"reward: {total_reward}")
-    ascab_.render()
+    ascab.render()
 
 
 def cheating_agent():
@@ -35,7 +34,7 @@ def cheating_agent():
     total_reward = 0.0
     while not terminated:
         action = 0.0
-        if ascab.result_data['Risk'] and ascab.result_data['Risk'][-1] > 0.05: action = 1.0
+        if ascab.get_wrapper_attr('result_data')['Risk'] and ascab.get_wrapper_attr('result_data')['Risk'][-1] > 0.05: action = 1.0
         _, reward, terminated, _, _ = ascab.step(action)
         total_reward += reward
     print(f"reward: {total_reward}")
@@ -54,6 +53,6 @@ def zero_agent():
 
 
 if __name__ == "__main__":
-    zero_agent() #-0.634
-    cheating_agent()
+    #zero_agent() #-0.634
+    #cheating_agent()
     rl_agent()

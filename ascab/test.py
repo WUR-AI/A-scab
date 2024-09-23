770eaeeae7fb656e10a89728f4ee2d798f490a5b
Michiel Kallenberg <michielkallenberg@gmail.com>
2024-09-23 13:09:57 +0200

    make sb3 optional

--- ascab/test.py
+++ ascab/test.py
@@ -1,22 +1,26 @@
 import pandas as pd
 import datetime
+import matplotlib
+matplotlib.use('TkAgg')
+from gymnasium.wrappers import FlattenObservation, FilterObservation
 
 from ascab.utils.weather import get_meteo, summarize_rain
 from ascab.utils.plot import plot_precipitation_with_rain_event
 from ascab.env.env import AScabEnv
 
-from stable_baselines3.common.env_checker import check_env
-from gymnasium.wrappers import FlattenObservation, FilterObservation
+try:
+    from stable_baselines3.common.env_checker import check_env
+except ImportError:
+    check_env = None
 
-import matplotlib
 
-matplotlib.use('TkAgg')
+
 
 params = {
-    "latitude": 50.8,
-    "longitude": 5.2,
-    "start_date": "2011-02-01",
-    "end_date": "2011-08-01",
+    "latitude": 42.16200214762477,
+    "longitude": 3.092425100060547,
+    "start_date": "2022-08-15",
+    "end_date": "2022-08-18",
     "hourly": ["temperature_2m", "relative_humidity_2m", "precipitation", "vapour_pressure_deficit", "is_day"],
     "timezone": "auto"
 }
@@ -41,7 +45,10 @@ def test_environment():
     ascab = AScabEnv()
     ascab = FilterObservation(ascab, filter_keys=["weather", "tree"])
     ascab = FlattenObservation(ascab)
-    check_env(ascab)
+    if check_env is not None:
+        check_env(ascab)
+    else:
+        print("Stable-baselines3 is not installed. Skipping environment check.")
 
 
 if __name__ == "__main__":
--- ascab/train.py
+++ ascab/train.py
@@ -1,8 +1,13 @@
-from stable_baselines3 import PPO
+import datetime
 from gymnasium.wrappers import FlattenObservation, FilterObservation
+
 from ascab.utils.plot import plot_results
 from ascab.env.env import AScabEnv
-import datetime
+
+try:
+    from stable_baselines3 import PPO
+except ImportError:
+    PPO = None
 
 
 def get_default_observation_filter():
@@ -15,6 +20,11 @@ def rl_agent(
     observation_filter=get_default_observation_filter(),
     render=True,
 ):
+    if PPO is None:
+        raise ImportError(
+            "stable-baselines3 is not installed. Please install it to use the rl_agent."
+        )
+
     if ascab is None:
         ascab = AScabEnv()
     if observation_filter:
@@ -72,7 +82,9 @@ def zero_agent(ascab: AScabEnv = None, render=True):
     return ascab.get_info(to_dataframe=True)
 
 
-def fixed_schedule_agent(ascab: AScabEnv = None, dates: list[datetime.date] = None, render=True):
+def fixed_schedule_agent(
+    ascab: AScabEnv = None, dates: list[datetime.date] = None, render=True
+):
     if ascab is None:
         ascab = AScabEnv()
     if dates is None:
@@ -102,6 +114,9 @@ if __name__ == "__main__":
     ascab_zero = zero_agent(ascab_env)  # -0.634
     print("cheating agent")
     ascab_cheating = cheating_agent(ascab_env)
-    print("rl agent")
-    ascab_rl = rl_agent(ascab_env, 5)
-    plot_results({"zero": ascab_zero, "cheater": ascab_cheating, "rl_agent": ascab_rl})
+    if PPO is not None:
+        print("rl agent")
+        ascab_rl = rl_agent(ascab_env, 5)
+        plot_results({"zero": ascab_zero, "cheater": ascab_cheating, "rl_agent": ascab_rl})
+    else:
+        print("Stable-baselines3 is not installed. Skipping RL agent.")
